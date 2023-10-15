14cbed7ece89479e8bf5d8047c722e171f61638e
metaknowledge <danielh5@uw.edu>
2023-10-15 10:11:27 -0700

    added some text

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import store from './store';
 function App() {
   return (
     <Provider store={store}>
+      <header>
+        <h1 className="text-xl">Course Explorer</h1>
+      </header>
+      <hr></hr>
       <div className="py-20 w-[70%] m-auto">
         <CoursesList />
       </div>
+      <footer>made with love by Alexander Boyko, Daniel Hughes, and Julian Del Pino</footer>
     </Provider>
   );
 }
