35f4389f5101d0ca879bfc97a54e136530a46a28
Julian <Delpjul03@outlook.com>
2023-10-15 01:06:13 -0700

    gives course link,name,assignments,and assignment info

--- python/cse.py
+++ python/cse.py
@@ -3,6 +3,8 @@ from requests import Response
 from bs4 import BeautifulSoup
 from bs4 import NavigableString, Tag
 from enum import Enum
+import xmltojson
+import json
 
 
 
@@ -21,6 +23,7 @@ for course in courses:
   ref = menu.find('a', href=True)
   website_link = URL + ref['href'][3:]
   print(website_link)
+  
 
 #takes course link and goes to the assignement page
   classPage: Response = session.get(website_link)
@@ -28,14 +31,36 @@ for course in courses:
   assLink = soup.findAll('a', class_='nav-link')
 
   #course sites aren't uniform(331 homework html is in a different spot)
-  assPage = assLink[2]['href']
-  
-#gets the assignment info from page
+  try:
+    assPage = assLink[2]['href']
+  except IndexError:
+    break
+
+#gets the assignment info from page(theres probably a better way to do this)
   page = requests.get(assPage)
   soup = BeautifulSoup(page.content,'html.parser')
   hws = soup.find_all("div", class_='homeworks')
+
+  #give button link to actual assignment 
+  details = soup.find("a", class_="btn btn-373")
+  courseName = soup.find("a").find(text=True, recursive=False)
+  print(courseName)
+  print(details)
+
+
   for hw in hws:
-    print(hw)
+   deadlines = hw.find_all("b")
+   for deadline in deadlines:
+     
+     #assignment deadline
+     print(deadline)
+
+   for name in hw.find_all("h5",class_="card-header"):
+     for i in name:
+      #assignment name
+      print(i)
+  print()
+    
 
 
   
